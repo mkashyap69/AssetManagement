c17be778954c314fab3081d7e6a505b52711dc8b
Manish <manish.@Manishs-MacBook-Air.local>
2021-07-22 22:32:25 +0530

    setting up server

--- server/server.js
+++ server/server.js
@@ -1,23 +1,26 @@
 const express = require('express');
-const xss = require('xss');
-const mongoSanitize = require('mongo-sanitize');
+const hpp = require('hpp');
+const mongoSanitize = require('express-mongo-sanitize');
 const cors = require('cors');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const cluster = require('cluster');
 const { cpus } = require('os');
+require('dotenv').config();
 
-const app = express();
 const port = process.env.PORT || 9000;
 process.env.UV_THREADPOOL_SIZE = 8;
 
-app.use(xss());
+const app = express();
+
+app.use(hpp());
 app.use(helmet());
 app.use(mongoSanitize());
 app.use(cors());
+app.use(express.json());
 
 if (cluster.isMaster) {
-  const num_cpus = require('os').cpus.length;
+  const num_cpus = cpus().length;
 
   for (let i = 0; i < num_cpus; i++) {
     cluster.fork();
